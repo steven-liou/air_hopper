2b6339a72a952efb014dbe36be6b727021cc6169
Steven Liou <stevenliou@gmail.com>
2022-04-06 20:10:58 -0400

    Present airlines and airports in human friendly style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import data from './data';
+const {routes, airlines, airports, getAirlineById, getAirportByCode} = data;
+
+const Route = ({airline, src, dest}) => {
+  airline = getAirlineById(airline).name;
+  src = getAirportByCode(src).name;
+  dest = getAirportByCode(dest).name;
+  return (
+    <tr>
+      <th scope='row'>{airline}</th>
+      <td>{src}</td>
+      <td>{dest}</td>
+    </tr>
+  );
+};
+
+const Routes = ({routes}) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th scope='col'>Airline</th>
+          <th scope='col'>Origin</th>
+          <th scope='col'>Destination</th>
+        </tr>
+      </thead>
+      <tbody>
+        {routes.map((route, index) => {
+          return <Route key={index} {...route} />;
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+  <div className='app'>
+    <header className='header'>
+      <h1 className='title'>Airline Routes</h1>
+    </header>
+    <section>
+      <Routes routes={routes} />
+    </section>
+  </div>
+);
 
 export default App;
